045f8d8b4163c1c3f8ab9f6435146f4b92fb2ff1
cfgexe <65036495+cfgexe@users.noreply.github.com>
2021-10-02 03:28:45 +1000

    Update hackingpanel.bat

--- hackingpanel.bat
+++ hackingpanel.bat
@@ -11,7 +11,6 @@ ECHO ALTSEC AMONG US POP IT FIGET TOY Pinger
 set /p IP=Enter IP To Ping: 
 :top
 PING -n 1 %IP% | FIND "TTL="
-ECHO SKID ALERT BOZO GO LEARN HOW TO CODE LMAO 💀🙏🤡
 title Hacking: %IP% 
 IF ERRORLEVEL 1 (echo ip is offline get hacked monkey)
 ping -t 2 0 10 127.0.0.1 >nul
